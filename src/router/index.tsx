50efb5a339f38cbbb3abba4f27efb9f8e406e3c8
kuaiq <kuaiq@xiaopeng.com>
2022-01-27 09:28:25 +0800

    feat: init project

---
+++ src/router/index.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HomePage from '@/pages/Home'
+import NotFoundPage from '@/pages/Page404'
+
+const routes = [
+  { path: '/', element: <HomePage /> },
+  { path: '*', element: <NotFoundPage /> },
+]
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route path={path} element={element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  )
+}
